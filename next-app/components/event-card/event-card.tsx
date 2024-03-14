ee597cd96cb059724bfc6bc8d8a1e948860dba21
Chris Meurink <chris@Chriss-MacBook-Air.local>
2024-03-14 15:20:25 +0100

    Add tailwind and nextui

--- next-app/components/event-card/event-card.tsx
+++ next-app/components/event-card/event-card.tsx
@@ -1,32 +1,41 @@
+'use client';
+
 import { EventData } from "@/types/event";
 import style from "./event-card.module.css";
 import slugify from "@/utils/slugify";
-import Link from "next/link";
+
+
+import {Card, CardHeader, CardBody, CardFooter, Divider, Image, Link} from "@nextui-org/react";
 
 export default function EventCard({ event }: { event: EventData }) {
   return (
-    <div className={style.card}>
-      <Link href={`/events/${slugify(event.title)}/${event.id}`}>
-        <h3>{event.title}</h3>
-      </Link>
-      {event.description && <div>{event.description}</div>}
-      {event.date && (
-        <span>
-          {Intl.DateTimeFormat("nl-nl", { dateStyle: "long" }).format(
-            new Date(event.date)
-          )}
-        </span>
-      )}
-      {event.location && <div>{event.location.name}</div>}
-      {event.tags && (
-        <div>
-          {event.tags.map((tag) => (
-            <Link href={`/tags/${slugify(tag)}`} className="button" key={tag}>
-              {tag}
-            </Link>
-          ))}
+    <Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="nextui logo"
+          height={40}
+          radius="sm"
+          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">{event.title}</p>
+          <p className="text-small text-default-500">{event.date.toLocaleDateString()}</p>
         </div>
-      )}
-    </div>
-  );
+      </CardHeader>
+      <Divider/>
+      <CardBody>
+        <p>{event.description}</p>
+      </CardBody>
+      <Divider/>
+      <CardFooter>
+        <Link
+          showAnchorIcon
+          href={`/events/${slugify(event.title)}/${event.id}`}
+        >
+          View event
+        </Link>
+      </CardFooter>
+    </Card>
+  )
 }
